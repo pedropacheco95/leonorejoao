552d9af19293716fc84d2ba083ba229ab984e154
pedropacheco95 <pedropacheco95@gmail.com>
2022-08-01 13:16:52 +0100

    Background

--- leonorejoao/tools/specific_info.py
+++ leonorejoao/tools/specific_info.py
@@ -1,9 +1,9 @@
 #This page serves as a tool to define specific information to be used in the website.
 name = 'leonorejoao'
-title = 'TERESINHA e FRANCISCO'
+title = 'Leonor e João'
 mbway1 = '914973388'
 mbway2 = '91'
 iban = 'PT50'
 
 def generate_ty_contribution_message(person):
-    return 'Obrigado {person} pela contribuição! Beijinhos, Teresinha e Francisco'.format(person=person)
+    return 'Muito obrigado {person} pela contribuição! Beijinhos, Leonor e João'.format(person=person)
